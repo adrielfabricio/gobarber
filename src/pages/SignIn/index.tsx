a040d40e2b3c82eac0bf3e6e759f7120c6ba5a00
Adriel Fabrício <adrielfabricios@gmail.com>
2021-02-27 15:49:29 -0300

    feat(pages): add usability in SignIn screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,6 +5,7 @@ import {
 	Platform,
 	View,
 	ScrollView,
+	TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -27,6 +28,7 @@ import {
 
 const SignIn: React.FC = () => {
 	const formRef = useRef<FormHandles>(null);
+	const passwordInputRef = useRef<TextInput>(null);
 	const navigation = useNavigation();
 
 	const handleSignIn = useCallback((data: object) => {
@@ -52,8 +54,29 @@ const SignIn: React.FC = () => {
 						</View>
 
 						<Form ref={formRef} onSubmit={handleSignIn}>
-							<Input name="email" icon="mail" placeholder="E-mail" />
-							<Input name="password" icon="lock" placeholder="Senha" />
+							<Input
+								name="email"
+								icon="mail"
+								placeholder="E-mail"
+								autoCorrect={false}
+								keyboardType="email-address"
+								autoCapitalize="none"
+								returnKeyType="next"
+								onSubmitEditing={() => {
+									passwordInputRef.current?.focus();
+								}}
+							/>
+							<Input
+								ref={passwordInputRef}
+								name="password"
+								icon="lock"
+								placeholder="Senha"
+								secureTextEntry
+								returnKeyType="send"
+								onEndEditing={() => {
+									formRef.current?.submitForm();
+								}}
+							/>
 
 							<View>
 								<Button
