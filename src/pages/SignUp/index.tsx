d369f5bb7907558fc00fac6d29424af53f6d7eb0
Adriel Fabrício <adrielfabricios@gmail.com>
2021-02-27 11:21:34 -0300

    feat(routes): finish auth routing

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -7,6 +7,7 @@ import {
 	ScrollView,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -21,6 +22,8 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+	const navigation = useNavigation();
+
 	return (
 		<>
 			<KeyboardAvoidingView
@@ -48,7 +51,7 @@ const SignIn: React.FC = () => {
 				</ScrollView>
 			</KeyboardAvoidingView>
 
-			<BackToSignInButton onPress={() => {}}>
+			<BackToSignInButton onPress={() => navigation.goBack()}>
 				<Icon name="arrow-left" size={20} color="#fff" />
 				<BackToSignInButtonText>Voltar para logon</BackToSignInButtonText>
 			</BackToSignInButton>
